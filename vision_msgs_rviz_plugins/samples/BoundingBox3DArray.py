efc8b3eb11d923e7e5e2cad2c611b71caf19594c
Georg Novotny <georg.novotny@aon.at>
2023-01-27 20:43:14 +0100

    Adapt copyright + changed for yaml_cpp_vendor

--- vision_msgs_rviz_plugins/samples/BoundingBox3DArray.py
+++ vision_msgs_rviz_plugins/samples/BoundingBox3DArray.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright 2023 Open Source Robotics Foundation, Inc.
+# Copyright 2023 Georg Novotny
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
