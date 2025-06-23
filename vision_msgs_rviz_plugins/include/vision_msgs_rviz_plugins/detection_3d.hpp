7f9abee3c987e350f2e8add61c8970e1caf8c599
Alejandro Hernández Cordero <ahcorde@gmail.com>
2025-06-23 20:49:40 +0200

    Added lifetime field (#108)
    * Added lifetime field
    
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>
    
    * Added lifetime to the rest of plugins
    
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>
    
    ---------
    
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>
    (cherry picked from commit d45e9ff342cd0edf338d3eef29bdba87d6bfbc05)

--- vision_msgs_rviz_plugins/include/vision_msgs_rviz_plugins/detection_3d.hpp
+++ vision_msgs_rviz_plugins/include/vision_msgs_rviz_plugins/detection_3d.hpp
@@ -19,6 +19,7 @@
 #include <memory>
 #include <rviz_common/display.hpp>
 #include <rviz_common/properties/bool_property.hpp>
+#include <rviz_common/properties/int_property.hpp>
 #include <rviz_common/properties/float_property.hpp>
 #include <rviz_default_plugins/displays/marker/marker_common.hpp>
 #include <rviz_default_plugins/displays/marker_array/marker_array_display.hpp>
@@ -66,12 +67,14 @@ protected:
   bool only_edge_, show_score_;
   rviz_common::properties::BoolProperty * only_edge_property_;
   rviz_common::properties::FloatProperty * line_width_property_;
+  rviz_common::properties::IntProperty * lifetime_property_;
   rviz_common::properties::FloatProperty * alpha_property_;
   rviz_common::properties::BoolProperty * show_score_property_;
 
 protected Q_SLOTS:
   void updateEdge();
   void updateLineWidth();
+  void updateLifetime();
   void updateAlpha();
   void updateShowScores();
   void updateColorConfigs();
--- vision_msgs_rviz_plugins/src/detection_3d_array.cpp
+++ vision_msgs_rviz_plugins/src/detection_3d_array.cpp
@@ -32,6 +32,8 @@ Detection3DArrayDisplay::Detection3DArrayDisplay()
   string_property_ = new rviz_common::properties::StringProperty(
     "ConfigPath", "", "Path to yaml config for rgb color mappings", this,
     SLOT(updateColorConfigs()));
+  lifetime_property_ = new rviz_common::properties::IntProperty(
+    "Lifetime", 0, "Detection 3D array lifetime", this, SLOT(updateLifetime()));
 }
 
 Detection3DArrayDisplay::~Detection3DArrayDisplay()
@@ -59,6 +61,7 @@ void Detection3DArrayDisplay::onInitialize()
 
   line_width = line_width_property_->getFloat();
   alpha = alpha_property_->getFloat();
+  lifetime = lifetime_property_->getInt();
 
   only_edge_ = only_edge_property_->getBool();
   show_score_ = show_score_property_->getBool();
@@ -114,6 +117,14 @@ void Detection3DArrayDisplay::updateEdge()
 void Detection3DArrayDisplay::updateLineWidth()
 {
   line_width = line_width_property_->getFloat();
+  if (latest_msg) {
+    processMessage(latest_msg);
+  }
+}
+
+void Detection3DArrayDisplay::updateLifetime()
+{
+  lifetime = lifetime_property_->getInt();
   if (latest_msg) {
     processMessage(latest_msg);
   }
